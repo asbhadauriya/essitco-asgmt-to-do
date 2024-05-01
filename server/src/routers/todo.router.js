2ee8e5d07c6a868b7532aa2d2f5edc3e17daedc5
Akshay Bhadauriya <64069106+asbhadauriya@users.noreply.github.com>
2024-05-01 18:34:39 +0530

    changes

--- server/src/routers/todo.router.js
+++ server/src/routers/todo.router.js
@@ -1,11 +1,11 @@
-const todoRouter = require('express').Router();
-const authController = require('../controllers/authController');
-const Controller = require('../controllers/index')
-const {todoController} = Controller.module
-const authMiddleware = require('../middleware/authenticate');
+const todoRouter = require("express").Router();
+const authController = require("../controllers/authController");
+const Controller = require("../controllers/index");
+const { todoController } = Controller.module;
+const authMiddleware = require("../middleware/authenticate");
 
-todoRouter.post('/create',authMiddleware, todoController.createTodo)
-todoRouter.get('/get-all',authMiddleware, todoController.getAllTodo)
-todoRouter.put('/todoLists/:id', authMiddleware,todoController.updateTodo);
+todoRouter.post("/create", authMiddleware, todoController.createTodo);
+todoRouter.get("/get-all", authMiddleware, todoController.getAllTodo);
+todoRouter.put("/update/:id", authMiddleware, todoController.updateTodo);
 // router.delete('/todoLists/:id', authMiddleware,todoController.deleteTodoList);
-module.exports = todoRouter;   
+module.exports = todoRouter;
